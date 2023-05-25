52a19ab88a3db094d8bfa42b123b7afb9f32fa59
Paul Groß <Grossp@students.uni-marburg.de>
2023-05-25 15:31:06 +0200

    added cost function for paths

--- Project/src/main/java/org/example/logic/tools/algorithms/GroupMatchingGraph.java
+++ Project/src/main/java/org/example/logic/tools/algorithms/GroupMatchingGraph.java
@@ -10,25 +10,29 @@ import java.util.List;
 
 public class GroupMatchingGraph {
 
-    public Graph<PairMatched> createGraph(List<PairMatched> matchedPairs){
+    public Graph<PairMatched> createGraph(List<PairMatched> matchedPairs) {
         PairMatched furthestPair = GetFurthestPair(matchedPairs);
         double maxDistanceToPartyLocation = furthestPair.distanceToPartyLocation;
+
+        for (int x = 0; x < matchedPairs.size(); x++) {
+
+            for (int y = x + 1; y < matchedPairs.size(); y++) {
+
+            }
+        }
 
         throw new RuntimeException();
     }
 
-    public double calcPathCosts(PairMatched pairA, PairMatched pairB) {
-        return Coordinate.getDistance(pairA.getKitchen().coordinate, pairB.getKitchen().coordinate);
+    public double calcPathCosts(PairMatched pairA, PairMatched pairB, double maxDistanceToPartyLocation) {
+        return Coordinate.getDistance(pairA.getKitchen().coordinate, pairB.getKitchen().coordinate) / (maxDistanceToPartyLocation * 2);
+
     }
 
-
-    public static PairMatched GetFurthestPair(List<PairMatched> pairs){
-        //System.out.println("matched pair list: " + pairs);
+    public static PairMatched GetFurthestPair(List<PairMatched> pairs) {
         pairs.sort((pairA, pairB) -> {
             return Double.compare(pairB.distanceToPartyLocation, pairA.distanceToPartyLocation);
         });
-
-        //pairs.forEach(System.out::println);
         return pairs.get(0);
     }
 
