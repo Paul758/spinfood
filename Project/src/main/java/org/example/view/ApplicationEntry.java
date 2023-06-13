428be174b346d0cfb8defe101bc0c07bfa950d27
Paul758 <paul.gross01@web.de>
2023-06-13 21:39:15 +0200

    Added functionality to show groups in tableView

--- Project/src/main/java/org/example/view/ApplicationEntry.java
+++ Project/src/main/java/org/example/view/ApplicationEntry.java
@@ -9,33 +9,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import org.example.data.DataManagement;
 import org.example.data.structures.Solo;
-import org.example.data.tools.CSVReader;
 import org.example.logic.metrics.PairMetrics;
+import org.example.logic.structures.GroupMatched;
 import org.example.logic.structures.MatchingRepository;
 import javafx.event.ActionEvent;
 import org.example.logic.structures.PairMatched;
+import org.example.view.tools.FileSelectionManager;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationEntry extends Application {
 
     @FXML
     private Button buttonFile;
-
     @FXML
     private ListView<Solo> listViewSoloRegistration;
 
-
+    //Matched pairs table
     @FXML
     private TableView<PairMatched> tableViewMatchedPairs;
-
     @FXML
     private TableColumn<PairMatched, String> tableColumnFirstPerson;
     @FXML
@@ -44,7 +41,6 @@ public class ApplicationEntry extends Application {
     private TableColumn<PairMatched, String> tableColumnFirstPersonAge;
     @FXML
     private TableColumn<PairMatched, String> tableColumnFirstPersonFoodPreference;
-
     @FXML
     private TableColumn<PairMatched, String> tableColumnSecondPerson;
     @FXML
@@ -53,7 +49,7 @@ public class ApplicationEntry extends Application {
     private TableColumn<PairMatched, String> tableColumnSecondPersonAge;
     @FXML
     private TableColumn<PairMatched, String> tableColumnSecondPersonFoodPreference;
-
+    //Pair Metrics
     @FXML
     private TableColumn<PairMatched, String> tableColumnAgeDifference;
     @FXML
@@ -61,10 +57,9 @@ public class ApplicationEntry extends Application {
     @FXML
     private TableColumn<PairMatched, String> tableColumnFoodPreferenceDeviation;
 
-
+    //Table view for unmatched Solos
     @FXML
     private TableView<Solo> tableViewUnmatchedSolos;
-
     @FXML
     private TableColumn<Solo, String> tableColumnUnmatchedPerson;
     @FXML
@@ -74,12 +69,38 @@ public class ApplicationEntry extends Application {
     @FXML
     private TableColumn<Solo, String> tableColumnUnmatchedPersonFoodPreference;
 
+    //Table view for matchedGroups
+    @FXML
+    private TableView<GroupMatched> tableViewGroupMatched;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnCookingPair;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnCookingPairGender;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnCookingPairAge;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnCookingPairFoodPreference;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnSecondPair;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnSecondPairGender;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnSecondPairAge;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnSecondPairFoodPreference;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnThirdPair;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnThirdPairGender;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnThirdPairAge;
+    @FXML
+    private TableColumn<GroupMatched, String> tableColumnThirdPairFoodPreference;
 
 
 
     @FXML
     private ListView<PairMatched> listViewPairs;
-
     Stage primaryStage;
     MatchingRepository matchingRepository;
 
@@ -100,14 +121,14 @@ public class ApplicationEntry extends Application {
 
     @FXML
     private void handleButtonFileClick(ActionEvent event) {
-        List<File> selectedFiles = selectFiles(primaryStage);
+        FileSelectionManager fileChooser = new FileSelectionManager(primaryStage);
+        List<File> selectedFiles = fileChooser.selectFiles();
         matchingRepository = new MatchingRepository(new DataManagement(selectedFiles.get(0).getAbsolutePath(), selectedFiles.get(1).getAbsolutePath()));
-        System.out.println(matchingRepository.getMatchedPairsCollection().toString());
-
 
         ShowParticipants();
         populatePairMatchedTable();
         populateUnmatchedSoloTable();
+        populateGroupMatchedTable();
     }
 
     private void ShowParticipants() {
@@ -134,6 +155,54 @@ public class ApplicationEntry extends Application {
     }
 
 
+    private void populateGroupMatchedTable() {
+
+        //first pair
+        tableColumnCookingPairGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getCook().getSoloA().person.sex().toString() + ", " +
+                data.getValue().getCook().getSoloB().person.sex().toString()));
+
+        tableColumnCookingPairAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getCook().getSoloA().person.age() + ", " +
+                data.getValue().getCook().getSoloB().person.age())));
+
+        tableColumnCookingPairFoodPreference.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getCook().getSoloA().foodPreference + ", " +
+                data.getValue().getCook().getSoloB().foodPreference)));
+
+        //second pair
+        tableColumnSecondPairGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPairList().get(1).getSoloA().person.sex().toString() + ", " +
+                data.getValue().getPairList().get(1).getSoloB().person.sex().toString()));
+
+        tableColumnSecondPairAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getPairList().get(1).getSoloA().person.age() + ", " +
+                data.getValue().getPairList().get(1).getSoloB().person.age())));
+
+        tableColumnSecondPairFoodPreference.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getPairList().get(1).getSoloA().foodPreference + ", " +
+                data.getValue().getPairList().get(1).getSoloB().foodPreference)));
+
+        //third pair
+        tableColumnThirdPairGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPairList().get(2).getSoloA().person.sex().toString() + ", " +
+                data.getValue().getPairList().get(2).getSoloB().person.sex().toString()));
+
+        tableColumnThirdPairAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getPairList().get(2).getSoloA().person.age() + ", " +
+                data.getValue().getPairList().get(2).getSoloB().person.age())));
+
+        tableColumnThirdPairFoodPreference.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getPairList().get(2).getSoloA().foodPreference + ", " +
+                data.getValue().getPairList().get(2).getSoloB().foodPreference)));
+
+        ObservableList<GroupMatched> pairMatchedObservableList = FXCollections.observableArrayList();
+        pairMatchedObservableList.addAll(matchingRepository.getMatchedGroupsCollection());
+
+        tableViewGroupMatched.setItems(pairMatchedObservableList);
+    }
+
+    private void populateUnmatchedSoloTable() {
+        tableColumnUnmatchedPersonGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().person.sex().toString()));
+        tableColumnUnmatchedPersonAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().person.age())));
+        tableColumnUnmatchedPersonFoodPreference.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().foodPreference.toString()));
+        ObservableList<Solo> soloObservableList = FXCollections.observableArrayList();
+        soloObservableList.addAll(matchingRepository.soloSuccessors);
+
+        tableViewUnmatchedSolos.setItems(soloObservableList);
+    }
+
     private void populatePairMatchedTable() {
         tableColumnFirstPersonGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getSoloA().person.sex().toString()));
         tableColumnFirstPersonAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getSoloA().person.age())));
@@ -152,42 +221,7 @@ public class ApplicationEntry extends Application {
         tableViewMatchedPairs.setItems(pairMatchedObservableList);
     }
 
-    private void populateUnmatchedSoloTable() {
-        tableColumnUnmatchedPersonGender.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().person.sex().toString()));
-        tableColumnUnmatchedPersonAge.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().person.age())));
-        tableColumnUnmatchedPersonFoodPreference.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().foodPreference.toString()));
-        ObservableList<Solo> soloObservableList = FXCollections.observableArrayList();
-        soloObservableList.addAll(matchingRepository.soloSuccessors);
 
-        tableViewUnmatchedSolos.setItems(soloObservableList);
-    }
-
-
-    private List<File> selectFiles(Stage primaryStage) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-        List<File> dataFiles = new ArrayList<>();
-        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(primaryStage);
-        File partyLocation = new File("-1");
-        File participantList = new File("-1");
-        if (selectedFiles != null) {
-            for (File file : selectedFiles) {
-                if(CSVReader.isPartyLocation(file)) {
-                    partyLocation = file;
-                } else {
-                    participantList = file;
-                }
-            }
-        } else {
-            System.out.println("no files selected");
-        }
-        System.out.println("The party location is " + partyLocation.getAbsolutePath());
-        System.out.println("The participant list is " + participantList.getAbsolutePath());
-        dataFiles.add(participantList);
-        dataFiles.add(partyLocation);
-        return dataFiles;
-
-    }
 
     public static void main(String[] args) {
         launch(args);
