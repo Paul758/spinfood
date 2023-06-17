6538a553e0a0447c6a8df1f54e0ea58372712e64
Felix Groß <Grossf@students.uni-marburg.de>
2023-06-17 12:23:30 +0200

    merged javafxTest

--- Project/src/main/java/org/example/SuperMain.java
+++ Project/src/main/java/org/example/SuperMain.java
@@ -4,7 +4,7 @@ import org.example.view.ApplicationEntry;
 
 // SuperMain is needed, because a jar executable needs a classpath to a Main class which doesn't extend another class
 // The only purpose of this class is to call the main Method of the Main that extends the Application class for JavaFX
-public class SuperMain {
+public class  SuperMain {
     public static void main(String[] args) {
         ApplicationEntry.main(args);
     }
