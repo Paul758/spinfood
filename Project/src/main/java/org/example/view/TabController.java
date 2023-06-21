ae2bdf8b2f39323ed955888345416c4655fbc389
Paul758 <paul.gross01@web.de>
2023-06-21 19:31:25 +0200

    small refactoring in TabController

--- Project/src/main/java/org/example/view/TabController.java
+++ Project/src/main/java/org/example/view/TabController.java
@@ -10,34 +10,26 @@ public abstract class TabController {
     ArrayDeque<UIAction> redoHistory = new ArrayDeque<>();
 
     public void undo(){
-        System.out.println("undo called");
-        System.out.println("redoHistorySize before undo is " + redoHistory.size());
+
         if(undoHistory.isEmpty()) {
             return;
         }
         UIAction lastAction = undoHistory.removeLast();
         redoHistory.addLast(lastAction);
         lastAction.undo();
-
         updateUI();
-        System.out.println("redoHistorySize after undo is " + redoHistory.size());
     }
 
     public void redo() {
-        System.out.println("redo called");
-        System.out.println("redo History now is sized: " + redoHistory.size() );
         if(redoHistory.isEmpty()) {
-            System.out.println("redoHistory is empty, returning nothing");
             return;
         }
         UIAction lastAction = redoHistory.removeLast();
         run(lastAction);
         updateUI();
-
     }
 
     public void run(UIAction action) {
-        System.out.println("current action is " + action);
         undoHistory.addLast(action);
         action.run();
         updateUI();
