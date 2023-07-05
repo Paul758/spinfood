ca148cac076d66800774c0b2daa50484cfd63629
Felix Groß <Grossf@students.uni-marburg.de>
2023-07-05 18:18:38 +0200

    deleted methods and classes in view.tools with no usage

--- Project/src/main/java/org/example/view/tools/ViewTools.java
+++ Project/src/main/java/org/example/view/tools/ViewTools.java
@@ -1,15 +1,10 @@
 package org.example.view.tools;
 
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.example.data.enums.FoodPreference;
-import org.example.data.structures.Solo;
-import org.example.logic.metrics.PairListMetrics;
-import org.example.logic.structures.PairMatched;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class ViewTools {
 
@@ -35,20 +30,5 @@ public class ViewTools {
         LocalDateTime timestamp = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         return timestamp.format(formatter);
-    }
-
-    public static String getSoloData(Solo solo) {
-        return solo.getPerson().name() + "\n"
-                + solo.getPerson().age() + "\n"
-                + solo.getPerson().sex() + "\n";
-
-    }
-
-    public static String getPairListMetrics(List<PairMatched> pairs) {
-        return "is valid: " + PairListMetrics.isValid(pairs)
-                + ", count pairs: " + pairs.size()
-                + ", age difference: " + PairListMetrics.calcAgeDifference(pairs)
-                + ", gender diversity: " + PairListMetrics.calcGenderDiversity(pairs)
-                + ", preference deviation: " + PairListMetrics.calcPreferenceDeviation(pairs);
     }
 }
